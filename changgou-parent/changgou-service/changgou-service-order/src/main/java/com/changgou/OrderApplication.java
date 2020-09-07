ae36a434d96ac43b0a5a08ff4b7192f0c61a6f99
xiekecheng <kc.xie@qq.com>
2020-09-07 08:06:09 +0800

    数据库修改
    数据库修改

---
+++ changgou-parent/changgou-service/changgou-service-order/src/main/java/com/changgou/OrderApplication.java
@@ -0,0 +1,16 @@
+package com.changgou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author xiekecheng
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class,args);
+    }
+}
